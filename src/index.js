c254e870e8ee792153768ae435c15c320d20a4d8
majed187 <majedshrndan45@gmail.com>
2018-09-25 17:51:22 +0200

    and is Working:)

--- src/index.js
+++ src/index.js
@@ -1,1 +1,31 @@
+#!/usr/bin/env node
+import axios from 'axios';
+const args = process.argv.slice(2);
 
+class Weather {
+    constructor(city) {
+        this.apiKey = '638df99df6885ac7bc3b947ac52d7719';
+        this.baseURL = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${
+            this.apiKey
+        }`;
+    }
+    async weather() {
+        try {
+            const res = await axios.get(`${this.baseURL}`);
+            const cond = res.data.weather
+                .map(item => {
+                    return item.description;
+                })
+                .join(' ,');
+            return ` The temperatur ${args} is ${res.data.main.temp}°C ${cond}`;
+        } catch (error) {
+            console.error(error);
+        }
+    }
+}
+const weather = new Weather(args[0]);
+
+weather
+    .weather()
+    .then(console.log)
+    .catch(console.error);
